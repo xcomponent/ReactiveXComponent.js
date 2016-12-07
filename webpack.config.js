23bb6eabc4a8c449aed872393b9f51048e0c8bcb
nicolaserny <nicolas.erny@xcomponent.com>
2016-12-07 14:32:47 +0100

    Snapshot: add stateMachineRefs in the response

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,30 @@ var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
   entry: APP_DIR + '/index.js',
+  devtool: 'cheap-module-source-map',
   output: {
     path: BUILD_DIR,
     filename: 'xcomponentapi.js',
     libraryTarget: 'umd',
-    library: 'xcomponentapi',    
+    library: 'xcomponentapi',
     publicPath: './'
   },
-  module : {
-    loaders : []
+  plugins: process.env.NODE_ENV === 'production' ? [    
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ] : [],
+  module: {
+    loaders: []
   },
   externals: {
     'websocket': 'websocket',
