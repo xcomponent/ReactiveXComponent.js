ab1b01367595706a330716bb6c7b5868e5f7d149
mbarekh <hazem.mbarek@hotmail.com>
2016-12-07 14:22:05 +0100

    handle unexpected connection lost

--- src/communication/xcSession.js
+++ src/communication/xcSession.js
@@ -1,9 +1,8 @@
-
 define(["../javascriptHelper", "../guid", "./xcWebSocketPublisher", "./xcWebSocketSubscriber", "../configuration/xcWebSocketBridgeConfiguration"],
-    function (javascriptHelper, Guid, Publisher, Subscriber, xcWebSocketBridgeConfiguration) {
+    function(javascriptHelper, Guid, Publisher, Subscriber, xcWebSocketBridgeConfiguration) {
         "use strict";
 
-        var SessionFactory = function (serverUrl, configuration) {
+        var SessionFactory = function(serverUrl, configuration) {
             var WebSocket = javascriptHelper.getJavascriptHelper().WebSocket;
             var webSocket = new WebSocket(serverUrl);
             var session = new Session(serverUrl, webSocket, configuration);
@@ -11,7 +10,7 @@ define(["../javascriptHelper", "../guid", "./xcWebSocketPublisher", "./xcWebSock
         }
 
 
-        var Session = function (serverUrl, webSocket, configuration) {
+        var Session = function(serverUrl, webSocket, configuration) {
             this.serverUrl = serverUrl;
             this.webSocket = webSocket;
             this.configuration = configuration;
@@ -24,7 +23,7 @@ define(["../javascriptHelper", "../guid", "./xcWebSocketPublisher", "./xcWebSock
         }
 
 
-        Session.prototype.setPrivateTopic = function (privateTopic) {
+        Session.prototype.setPrivateTopic = function(privateTopic) {
             var kindPrivate = xcWebSocketBridgeConfiguration.kinds.Private;
             this.privateSubscriber.sendUnsubscribeRequestToTopic(this.privateTopic, kindPrivate);
             this.privateTopic = privateTopic;
@@ -38,44 +37,47 @@ define(["../javascriptHelper", "../guid", "./xcWebSocketPublisher", "./xcWebSock
         }
 
 
-        Session.prototype.init = function (sessionListener) {
+        Session.prototype.init = function(sessionListener) {
             var thisObject = this;
 
-            this.webSocket.onopen = function (e) {
+            this.webSocket.onopen = function(e) {
                 thisObject.privateSubscriber.sendSubscribeRequestToTopic(thisObject.privateTopic, xcWebSocketBridgeConfiguration.kinds.Private);
                 console.log("connection started on " + thisObject.serverUrl + ".");
                 sessionListener(null, thisObject);
             }
 
-            this.webSocket.onerror = function (e) {
+            this.webSocket.onerror = function(e) {
                 var messageError = "Error on " + thisObject.serverUrl + ".";
                 console.error(messageError);
                 console.error(e);
                 sessionListener(messageError, null);
             }
 
-            this.webSocket.onclose = function (e) {
+            this.webSocket.onclose = function(e) {
                 console.log("connection on " + thisObject.serverUrl + " closed.");
                 console.log(e);
+                if (!e.wasClean) {
+                    throw new Error("unexpected connection close with code " + e.code);
+                }
             }
         }
 
 
-        Session.prototype.createPublisher = function () {
+        Session.prototype.createPublisher = function() {
             var publisher = new Publisher(this.webSocket, this.configuration, this.privateTopic);
             this.publishers.push(publisher);
             return publisher;
         }
 
 
-        Session.prototype.createSubscriber = function () {
+        Session.prototype.createSubscriber = function() {
             var subscriber = new Subscriber(this.webSocket, this.configuration, this.replyPublisher, this.guid);
             this.subscribers.push(subscriber);
             return subscriber;
         }
 
 
-        Array.prototype.removeElement = function (e, msg) {
+        Array.prototype.removeElement = function(e, msg) {
             var index = this.indexOf(e);
             if (index > -1) {
                 this.splice(index, 1);
@@ -85,17 +87,17 @@ define(["../javascriptHelper", "../guid", "./xcWebSocketPublisher", "./xcWebSock
         }
 
 
-        Session.prototype.disposePublisher = function (publisher) {
+        Session.prototype.disposePublisher = function(publisher) {
             this.publishers.removeElement(publisher, 'Publisher not found');
         }
 
 
-        Session.prototype.disposeSubscriber = function (subscriber) {
+        Session.prototype.disposeSubscriber = function(subscriber) {
             this.subscribers.removeElement(subscriber, 'Subscriber not found');
         }
 
 
-        Session.prototype.dispose = function () {
+        Session.prototype.dispose = function() {
             for (var i = 0; i < this.publishers.length; i++) {
                 this.disposePublisher(this.publishers[i]);
             }
@@ -105,7 +107,7 @@ define(["../javascriptHelper", "../guid", "./xcWebSocketPublisher", "./xcWebSock
         }
 
 
-        Session.prototype.close = function () {
+        Session.prototype.close = function() {
             this.privateSubscriber.sendUnsubscribeRequestToTopic(this.privateTopic, xcWebSocketBridgeConfiguration.kinds.Private);
             this.dispose();
             this.webSocket.close();
