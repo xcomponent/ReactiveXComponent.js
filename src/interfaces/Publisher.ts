9dd8a015ce5dca7d26bed37d5e0b471f7e75bf18
Julien MOLINA <julien@molina.pro>
2017-12-08 15:59:57 +0100

    Refactoring (#105)

--- src/interfaces/Publisher.ts
+++ src/interfaces/Publisher.ts
@@ -1,9 +1,10 @@
 import { ApiConfiguration } from "../configuration/apiConfiguration";
+import { StateMachineRef } from "../communication/xcomponentMessages";
 
 export interface Publisher {
     configuration: ApiConfiguration;
     privateTopic: string;
-    sendWithStateMachineRef(stateMachineRef: any, messageType: string, jsonMessage: any, visibilityPrivate: boolean, specifiedPrivateTopic: string): void;
+    sendWithStateMachineRef(stateMachineRef: StateMachineRef, messageType: string, jsonMessage: any, visibilityPrivate: boolean, specifiedPrivateTopic: string): void;
     send(componentName: string, stateMachineName: string, messageType: string, jsonMessage: any, visibilityPrivate: boolean, specifiedPrivateTopic: string): void;
     canPublish(componentName: string, stateMachineName: string, messageType: string): boolean;
 }
