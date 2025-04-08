9b858cde9b0fdfaa85b7f147e860267c4c4e5dca
fredericcarre <frederic.carre@xcomponent.com>
2025-04-08 19:55:31 +0200

    crypto patch

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,3 +1,13 @@
+
 export function generateUUID(): string {
+  if (typeof crypto !== "undefined" && typeof crypto.randomUUID === "function") {
     return crypto.randomUUID();
+  }
+
+  // Fallback (non cryptographique)
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = Math.random() * 16 | 0;
+    const v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
